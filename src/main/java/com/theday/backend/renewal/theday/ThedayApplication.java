f50d11e28cc27dee750fc9136b4a2cab1ddac3b5
Lee Sang Min <ehdqn118@naver.com>
2021-09-16 15:44:53 +0900

    - test config2

--- src/main/java/com/theday/backend/renewal/theday/ThedayApplication.java
+++ src/main/java/com/theday/backend/renewal/theday/ThedayApplication.java
@@ -2,12 +2,12 @@ package com.theday.backend.renewal.theday;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ThedayApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ThedayApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(ThedayApplication.class, args);
+	}
 }
