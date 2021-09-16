2119f281b228a12833f3663cccbdb79422a08427
Lee Sang Min <ehdqn118@naver.com>
2021-09-16 12:09:31 +0900

    - test config

---
+++ src/main/java/com/theday/backend/renewal/theday/ThedayApplication.java
@@ -0,0 +1,13 @@
+package com.theday.backend.renewal.theday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThedayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThedayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/theday/backend/renewal/theday/ThedayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theday.backend.renewal.theday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThedayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
